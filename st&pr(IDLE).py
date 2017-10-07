1dc2808722f75dc3579c830c5bce293eeeef7032
Miko Dimov <mdimov@bu.edu>
2017-10-07 22:03:36 +0200

    adding clunky prototype with basic sqlite3

---
+++ st&pr(IDLE).py
@@ -0,0 +1,51 @@
+# sqlite3 commands taken from https://docs.python.org/2/library/sqlite3.html
+
+import sqlite3
+
+# initialize databse
+conn = sqlite3.connect('stateVSproductivity_DATA.db')
+c = conn.cursor()
+
+def get_new_data():
+
+    day = str(input("Day of the week: "))
+    date = str(input("Date: "))
+    wake_up_time = input("Wake-up time: ")
+    bedtime_that_night = input("Bedtime that night: ")
+    total_sleep_hours_that_night = input("Total sleep hours that night: ")
+    sleep_quality_that_night = input("Sleep quality that night: ")
+    feeling = input("Feeling: ")
+    exercise = input("Exercise: ")
+    productivity = input("Productivity: ")
+
+    day_dataset = [day, date, wake_up_time, bedtime_that_night,
+                   total_sleep_hours_that_night, sleep_quality_that_night,
+                   feeling, exercise, productivity]
+
+    return day_dataset
+
+def initialize_database():
+    c.execute('''CREATE TABLE state_prod_RAW (day text, date text, wake_up_time real, 
+	bedtime_that_night real, total_sleep_hours_that_night real,
+	sleep_quality_that_night real, feeling real, exercise real,
+	productivity real)''')
+
+def modify_database():
+    new_dataset = get_new_data()
+    c.execute('INSERT INTO state_prod_RAW VALUES (?,?,?,?,?,?,?,?,?)', new_dataset)
+
+
+def print_database():
+    c.execute ('SELECT * FROM state_prod_RAW ') 
+    for row in c.fetchall():
+        print(row)
+
+def close_database():
+    # commit changes and close database
+    conn.commit()
+    conn.close()
+
+def add_new_dataset():
+    modify_database()
+    print_database()
+    close_database()
